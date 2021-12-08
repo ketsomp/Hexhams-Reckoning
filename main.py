afbfaabef1b0965cf2ebce86e37e7affa495f190
Slender <66839991+deathskull728@users.noreply.github.com>
2021-12-08 12:55:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+import pygame
+
+pygame.init()
+
+clock=pygame.time.Clock()
+fps=60
+
+# file paths
+IconPath = 'assets/mario.png'
+SpriteRImagePath = 'assets/firemario_R.png'
+SpriteLImagePath = 'assets/firemario_L.png'
+Enemy1ImagePath = 'assets/goomba.png'
+MusicPath = 'assets/mario_soundtrack.mp3'
+ProjectilePath = 'assets/fireball.png'
+ProjectileSoundEffectPath = 'assets/fireball.mp3'
+BackgroundPath = 'assets/grass_background.png'
+ShootingSoundEffectPath = 'assets/fireball.mp3'
+GameOverSoundEffectPath = 'assets/game_over_sound_effect.mp3'
+GameCompleteSoundEffectPath = 'assets/game_complete_sound_effect.mp3'
+
+#screen specifications
+screen_width=800
+screen_height=800
+icon=pygame.image.load(IconPath)
+screen=pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Hexham's Reckoning")
+pygame.display.set_icon(icon)
+
+#background
+BackgroundPath='assets/bg.png'
+backgroundimg=pygame.image.load(BackgroundPath)
+bg=pygame.transform.scale((backgroundimg),(screen_width,screen_height))
+
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+tile_size=40
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+
+        #load images
+        dirt_img = pygame.image.load('assets/dirt.png')
+        grass_img = pygame.image.load('assets/grass.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+world=World(world_data)
+
+#game loop
+running=True
+while running:
+    clock.tick(fps)
+    screen.blit(bg,(0,0))
+    world.draw()
+    draw_grid()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+    pygame.display.update()
