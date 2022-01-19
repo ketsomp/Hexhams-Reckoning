9747bc6f27406a30815824979de2b1392d2472df
Dweep Joshipura <dweepjoshipuracar@gmail.com>
2022-01-19 11:53:56 +0530

    Copied paths.py to levels

---
+++ assets/levels/paths.py
@@ -0,0 +1,31 @@
+from os import path
+
+Paths = {}
+Paths['Prefix'] = path.join(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))), 'assets')
+Paths['Icon'] = 'mario.png'
+Paths['SpriteRImage'] = path.join('mario_walking','mario6.png')
+Paths['SpriteLImage'] = 'firemario_L.png'
+Paths['Enemy1Image'] = 'goomba.png'
+Paths['Music1'] = 'mario_soundtrack.mp3'
+Paths['Projectile'] = 'fireball.png'
+Paths['ProjectileSoundEffect'] = 'fireball.mp3'
+Paths['ShootingSoundEffect'] = 'fireball.mp3'
+Paths['GameOverSoundEffect'] = 'game_over_sound_effect.mp3'
+Paths['GameCompleteSoundEffect'] = 'game_complete_sound_effect.mp3'
+Paths['Lava'] = 'lava.png'
+Paths['DeadPlayer'] = 'ghost.png'
+Paths['RestartButton'] = 'restart_btn.png'
+Paths['StartButton'] = 'start_btn.png'
+Paths['SaveButton'] = 'save_btn.png'
+Paths['LoadButton'] = 'load_btn.png'
+Paths['ExitButton'] = 'exit_btn.png'
+Paths['Exit'] = 'dirt.png'
+Paths['Coin'] = 'coin.png'
+Paths['CoinSFX'] = 'coin.wav'
+Paths['Background'] = 'bg.png'
+Paths['Grass'] = 'grass.png'
+Paths['Tree'] = 'tree.png'
+
+for k, v in Paths.items():
+    if k != 'Prefix':
+        Paths[k] = path.join(Paths['Prefix'], v)
