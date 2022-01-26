7797c43e9973d3320c531595e2ef6c49c6ce867f
Slender <66839991+deathskull728@users.noreply.github.com>
2022-01-26 20:51:12 +0530

    Update paths.py

--- paths.py
+++ paths.py
@@ -1,16 +1,16 @@
 from os import path
-
+# path.join(path.dirname(path.abspath(__file__)), 'assets')
 Paths = {}
 Paths['Prefix'] = path.join(path.dirname(path.abspath(__file__)), 'assets')
 Paths['Icon'] = 'mario.png'
 Paths['SpriteRImage'] = path.join('mario_walking','mario6.png')
 Paths['SpriteLImage'] = 'firemario_L.png'
 Paths['Enemy1Image'] = 'goomba.png'
-Paths['Music1'] = 'mario_soundtrack.mp3'
+Paths['Music1'] = 'medieval_ost.mp3'
 Paths['Projectile'] = 'fireball.png'
 Paths['ProjectileSoundEffect'] = 'fireball.mp3'
 Paths['ShootingSoundEffect'] = 'fireball.mp3'
-Paths['GameOverSoundEffect'] = 'game_over_sound_effect.mp3'
+Paths['GameOverSoundEffect'] = 'oof.mp3'
 Paths['GameCompleteSoundEffect'] = 'game_complete_sound_effect.mp3'
 Paths['Lava'] = 'lava.png'
 Paths['DeadPlayer'] = 'ghost.png'
